02c2a3add425508a04216702ba71fb23e6899ac1
isaacstclair <isaacstclair@gmail.com>
2021-05-13 12:34:43 -0600

    Project2 Updated--Final

---
+++ cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Project2/AddAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/AddAutomaton.cpp.obj"
+  "C:/Project2/ColonAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/ColonAutomaton.cpp.obj"
+  "C:/Project2/ColonDashAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/ColonDashAutomaton.cpp.obj"
+  "C:/Project2/CommaAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/CommaAutomaton.cpp.obj"
+  "C:/Project2/CommentAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/CommentAutomaton.cpp.obj"
+  "C:/Project2/DatalogProgram.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/DatalogProgram.cpp.obj"
+  "C:/Project2/FactsAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/FactsAutomaton.cpp.obj"
+  "C:/Project2/IDAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/IDAutomaton.cpp.obj"
+  "C:/Project2/LeftParenAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/LeftParenAutomaton.cpp.obj"
+  "C:/Project2/Lexer.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Lexer.cpp.obj"
+  "C:/Project2/MultiplyAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/MultiplyAutomaton.cpp.obj"
+  "C:/Project2/Parameter.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Parameter.cpp.obj"
+  "C:/Project2/Parser.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Parser.cpp.obj"
+  "C:/Project2/PeriodAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/PeriodAutomaton.cpp.obj"
+  "C:/Project2/Predicate.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Predicate.cpp.obj"
+  "C:/Project2/QMarkAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/QMarkAutomaton.cpp.obj"
+  "C:/Project2/QueriesAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/QueriesAutomaton.cpp.obj"
+  "C:/Project2/RightParenAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/RightParenAutomaton.cpp.obj"
+  "C:/Project2/Rule.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Rule.cpp.obj"
+  "C:/Project2/RulesAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/RulesAutomaton.cpp.obj"
+  "C:/Project2/SchemesAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/SchemesAutomaton.cpp.obj"
+  "C:/Project2/StringAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/StringAutomaton.cpp.obj"
+  "C:/Project2/Token.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Token.cpp.obj"
+  "C:/Project2/UndefinedAutomaton.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/UndefinedAutomaton.cpp.obj"
+  "C:/Project2/main.cpp" "C:/Project2/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project1_Starter_Code.dir/AddAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/ColonAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/ColonDashAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/CommaAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/CommentAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/DatalogProgram.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/FactsAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/IDAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/LeftParenAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Lexer.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/MultiplyAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Parameter.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Parser.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/PeriodAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Predicate.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/QMarkAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/QueriesAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/RightParenAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Rule.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/RulesAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/SchemesAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/StringAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Token.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/UndefinedAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/main.cpp.obj"
+  "Project1_Starter_Code.exe"
+  "Project1_Starter_Code.exe.manifest"
+  "Project1_Starter_Code.lib"
+  "Project1_Starter_Code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project1_Starter_Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
